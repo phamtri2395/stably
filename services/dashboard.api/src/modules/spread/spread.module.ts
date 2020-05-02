0c395be9e918a3cd01c0ca239952239ce221298d
Pham Minh Tri <phamtri2395@gmail.com>
2020-05-03 04:06:46 +0700

    feat(dashboard.api): provide API to get history of bid-ask spread

--- services/dashboard.api/src/modules/spread/spread.module.ts
+++ services/dashboard.api/src/modules/spread/spread.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { BidAskSpreadModel } from '@schemas';
 import { SpreadGateway } from './spread.gateway';
+import { SpreadService } from './spread.service';
+import { SpreadController } from './spread.controller';
 
 @Module({
-  providers: [SpreadGateway],
-  controllers: [],
+  imports: [MongooseModule.forFeature([BidAskSpreadModel])],
+  providers: [SpreadGateway, SpreadService],
+  controllers: [SpreadController],
   exports: [SpreadGateway],
 })
 export class SpreadModule {}
---
+++ services/dashboard.api/src/modules/spread/spread.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { BidAskSpread, BidAskSpreadModel } from '@schemas';
+
+@Injectable()
+export class SpreadService {
+  private readonly logger = new Logger(SpreadService.name);
+
+  constructor(@InjectModel(BidAskSpreadModel.name) private readonly bidAskSpreadModel: Model<BidAskSpread>) {}
+
+  public findAllBySymbol(symbol: string): Promise<BidAskSpread[]> {
+    return this.bidAskSpreadModel.find({ symbol }).sort({ createdAt: -1 }).exec();
+  }
+}
