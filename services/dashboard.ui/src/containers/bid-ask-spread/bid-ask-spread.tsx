c516369ec4948468ff8026800d764a70b2fb8e13
Pham Minh Tri <phamtri2395@gmail.com>
2020-05-04 06:47:23 +0700

    feat(dashboard.ui): update new spread via websocket

--- services/dashboard.ui/src/containers/bid-ask-spread/bid-ask-spread.tsx
+++ services/dashboard.ui/src/containers/bid-ask-spread/bid-ask-spread.tsx
@@ -4,20 +4,43 @@ import {
 } from 'antd';
 import { format } from 'date-fns/fp';
 
+import { SOCKET_SPREAD_EVENT } from '@common/constants';
 import { SupportedPairs } from '@common/configs';
-import { useGetSpread } from '@api';
+import { createSocketClient } from '@common/socket';
+import { useGetSpread, Spread } from '@api';
 
 import styles from './styles.scss';
 
 export const BidAskSpread: React.FC = () => {
   const [pair, setPair] = useState(SupportedPairs[0]);
   const [currentSpread, setCurrentSpread] = useState(Number.NaN);
-
-  const spreads = useGetSpread(pair).data || [];
+  const [spreads, setSpreads] = useState([]);
+  const firstFetchedSpreads = useGetSpread(pair).data || [];
 
   useEffect(() => {
-    if (spreads[0]) setCurrentSpread(spreads[0].spread);
-  }, [spreads]);
+    if (firstFetchedSpreads[0]) {
+      setSpreads(firstFetchedSpreads);
+      setCurrentSpread(firstFetchedSpreads[0].spread);
+    }
+  }, [firstFetchedSpreads]);
+
+  const handleSocketUpdate = (updatedSpread: Spread): void => {
+    const updatedSpreadHistory = [updatedSpread, ...spreads];
+    updatedSpreadHistory.pop();
+
+    setCurrentSpread(updatedSpread.spread);
+    setSpreads(updatedSpreadHistory);
+  };
+
+  useEffect(() => {
+    const spreadEvent = SOCKET_SPREAD_EVENT(pair);
+    const socket = createSocketClient();
+    socket.on(spreadEvent, handleSocketUpdate);
+
+    return (): void => {
+      socket.off(spreadEvent);
+    };
+  }, [pair, currentSpread]);
 
   const handleSelectPair = (selectedPair: string): void => {
     setPair(selectedPair);
@@ -42,7 +65,7 @@ export const BidAskSpread: React.FC = () => {
       </Col>
 
       <Col span={8}>
-        <Timeline>
+        <Timeline reverse pending="Updating...">
           {spreads.map((spread) => (
             // eslint-disable-next-line no-underscore-dangle
             <Timeline.Item key={spread._id}>
