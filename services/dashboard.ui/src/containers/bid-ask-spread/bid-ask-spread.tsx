6cd46499ce68e47c52bee24dd6ae46387791e8a2
Pham Minh Tri <phamtri2395@gmail.com>
2020-05-06 04:51:02 +0700

    Improve websocket performance (#11)
    * refactor(dashboard.ui): extendable api using swr
    
    * refactor(dashboard.ui): correctly handle websocket connection, save network resource

--- services/dashboard.ui/src/containers/bid-ask-spread/bid-ask-spread.tsx
+++ services/dashboard.ui/src/containers/bid-ask-spread/bid-ask-spread.tsx
@@ -3,54 +3,64 @@ import {
   Row, Col, Timeline, Statistic, Select,
 } from 'antd';
 import { format } from 'date-fns/fp';
+import { getOr, reverse, take } from 'lodash/fp';
 
 import { SOCKET_SPREAD_EVENT } from '@common/constants';
 import { SupportedPairs } from '@common/configs';
+import { prettyNumber } from '@common/utils';
 import { createSocketClient } from '@common/socket';
 import { useGetSpread, Spread } from '@api';
 
 import styles from './styles.scss';
 
+const socket = createSocketClient();
 export const BidAskSpread: React.FC = () => {
-  const [pair, setPair] = useState(SupportedPairs[0]);
-  const [currentSpread, setCurrentSpread] = useState(Number.NaN);
-  const [spreads, setSpreads] = useState([]);
-  const firstFetchedSpreads = useGetSpread(pair).data || [];
+  const [selectedPair, setSelectedPair] = useState(SupportedPairs[0]);
+  const [spreadHistory, setSpreadHistory] = useState([]);
+  const [socketAt, setSocketAt] = useState(Date.now());
+  const [socketData, setSocketData] = useState(null);
 
+  // socket handler
   useEffect(() => {
-    if (firstFetchedSpreads[0]) {
-      setSpreads(firstFetchedSpreads);
-      setCurrentSpread(firstFetchedSpreads[0].spread);
-    }
-  }, [firstFetchedSpreads]);
-
-  const handleSocketUpdate = (updatedSpread: Spread): void => {
-    const updatedSpreadHistory = [updatedSpread, ...spreads];
-    updatedSpreadHistory.pop();
-
-    setCurrentSpread(updatedSpread.spread);
-    setSpreads(updatedSpreadHistory);
-  };
-
-  useEffect(() => {
-    const spreadEvent = SOCKET_SPREAD_EVENT(pair);
-    const socket = createSocketClient();
-    socket.on(spreadEvent, handleSocketUpdate);
+    const event = SOCKET_SPREAD_EVENT(selectedPair);
+    socket.on(event, (data: Spread) => {
+      setSocketData(data);
+      setSocketAt(Date.now());
+    });
 
     return (): void => {
-      socket.off(spreadEvent);
+      socket.off(event);
+      setSocketData(null);
     };
-  }, [pair, currentSpread]);
+  }, [selectedPair]);
+  // clean up socket on unmount
+  useEffect(() => {
+    return (): void => {
+      socket.close();
+    };
+  }, []);
 
-  const handleSelectPair = (selectedPair: string): void => {
-    setPair(selectedPair);
+  // fetch bid-ask spread history
+  const { data: spreadHistoryData } = useGetSpread({ symbol: selectedPair, options: { revalidateOnFocus: false } });
+  useEffect(() => {
+    setSpreadHistory(spreadHistoryData);
+  }, [selectedPair, spreadHistoryData]);
+  const currentSpread = getOr(NaN, '0.spread', spreadHistory);
+
+  // update bid-ask spread via websocket
+  useEffect(() => {
+    if (socketData) setSpreadHistory(take(spreadHistory.length, [socketData, ...spreadHistory]));
+  }, [socketAt]);
+
+  const handleSelectPair = (pair: string): void => {
+    setSelectedPair(pair);
   };
 
   return (
     <Row>
       <Col span={16}>
         <Row className={styles.formItem}>
-          <Select defaultValue={pair} onSelect={handleSelectPair}>
+          <Select defaultValue={selectedPair} onSelect={handleSelectPair}>
             {SupportedPairs.map((supportedPair) => (
               <Select.Option key={supportedPair} value={supportedPair}>
                 {supportedPair}
@@ -60,16 +70,16 @@ export const BidAskSpread: React.FC = () => {
         </Row>
 
         <Row className={styles.formItem}>
-          <Statistic title="Current spread" value={currentSpread} />
+          <Statistic title="Current spread" value={prettyNumber(currentSpread)} />
         </Row>
       </Col>
 
       <Col span={8}>
         <Timeline reverse pending="Updating...">
-          {spreads.map((spread) => (
+          {reverse(spreadHistory).map((spread) => (
             // eslint-disable-next-line no-underscore-dangle
             <Timeline.Item key={spread._id}>
-              {spread.spread.toFixed(2)}
+              {prettyNumber(spread.spread)}
               <span> - </span>
               {format('kk:mm:ss', new Date(spread.createdAt))}
             </Timeline.Item>
